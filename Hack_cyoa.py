fda72a3ce12b7998ab962ba3acbedb129897ad9b
PureBlade9801 <31115609+PureBlade9801@users.noreply.github.com>
2022-11-05 15:21:29 -0400

    Sprites connected to the image and image moves to give some motion.

--- Hack_cyoa.py
+++ Hack_cyoa.py
@@ -18,6 +18,15 @@ class Player(pygame.sprite.Sprite):
         self.armor = 12
         self.Alive = True
         self.hit = 0
+        self.goDown = True
+        self.goUp = False
+
+
+        self.image = pygame.image.load("C:\\Users\\pureb\\Documents\\Some_python\\Ocean Blues\\Turtle.team\\assets\\astur\\merik_defaultA.png")
+
+    
+        self.rect = self.image.get_rect()
+        self.rect.center = [pos_x, pos_y]
 
     def attack(self):
         global playerDamage
@@ -35,22 +44,38 @@ class Player(pygame.sprite.Sprite):
                 print("You died!")
                 self.Alive = False
         
+        if self.goDown == True:
+            self.rect.y += 1
+            self.rect.x += 1
+            if self.rect.y >= 170:
+                self.goUp = True
+                self.goDown = False
+
+        if self.goUp == True:
+            self.rect.y -= 1
+            self.rect.x -= 1
+            if self.rect.y <= 150:
+                self.goUp = False
+                self.goDown = True
+            
+        
 class Enemy(pygame.sprite.Sprite):
-    def __init__(self, pos_x, pos_y, hp, defense, attack):
+    def __init__(self, pos_x, pos_y, hp, defense, attack, maxDmg):
         super().__init__()
         self.health = hp
         self.baseDamage = attack
         self.Alive = True
         self.armor = defense
+        self.maxDmg = maxDmg
         self.hit = 0
-
+    
     def attack(self):
         global enemyDamage
         self.hit = r.randrange(1,21) + self.baseDamage
         print(" ")
         print("The enemy rolled: " + str(self.hit))
         if self.hit >= merek.armor:
-            enemyDamage = (r.randrange(1,7) + self.baseDamage)
+            enemyDamage = (r.randrange(1,self.maxDmg) + self.baseDamage)
             merek.health = merek.health - enemyDamage
             print("The enemy dealt: " + str(enemyDamage))
     def update(self):
@@ -58,14 +83,15 @@ class Enemy(pygame.sprite.Sprite):
             if self.Alive == True:
                 print("You killed the enemy!")
                 self.Alive = False
+        
 
 
 
-merek = Player(0,0)
+merek = Player(480,180)
 player_group = pygame.sprite.Group()
 player_group.add(merek)
 
-mantaRay = Enemy(0,0, 20, 10, 2)
+mantaRay = Enemy(0,0, 20, 10, 2, 7)
 enemy_group = pygame.sprite.Group()
 enemy_group.add(mantaRay)
 
@@ -77,12 +103,14 @@ while True:
             pygame.quit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_SPACE:
-                if merek.health > 0:
-                    merek.attack()
                 if mantaRay.health > 0:
-                    mantaRay.attack()
+                    if merek.health > 0:
+                        merek.attack()
+                    if mantaRay.health > 0:
+                        mantaRay.attack()
 
     pygame.display.flip()
+    player_group.draw(screen)
     player_group.update()
     enemy_group.update()
-    clock.tick(60)
+    clock.tick(30)
