2cc77037720ddba0c9d5a1ea39ab0d2faec54533
PureBlade9801 <31115609+PureBlade9801@users.noreply.github.com>
2022-11-05 12:49:24 -0400

    Create Hack_cyoa.py

---
+++ Hack_cyoa.py
@@ -0,0 +1,13 @@
+import random as r
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((640,360))
+clock = pygame.time.Clock()
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+    print("hello")
